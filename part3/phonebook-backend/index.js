d90845356a0795d27bd27ca1fe45d34f0aa3a891
Danny Sun <sunnyhust2005@qq.com>
2023-11-10 14:27:12 +0800

    Revises 3.18 - Phonebook database step6

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -14,28 +14,6 @@ morgan.token('request-body', function getRequestBody(req) {
 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :request-body'));
 
-let persons = [
-  {
-    "id": 1,
-    "name": "Arto Hellas",
-    "number": "040-123456"
-  },
-  {
-    "id": 2,
-    "name": "Ada Lovelace",
-    "number": "39-44-5323523"
-  },
-  {
-    "id": 3,
-    "name": "Dan Abramov",
-    "number": "12-43-234345"
-  },
-  {
-    "id": 4,
-    "name": "Mary Poppendieck",
-    "number": "39-23-6423122"
-  }
-];
 
 app.get('/api/persons', (request, response) => {
   Person.find({}).then(result => {
@@ -44,7 +22,9 @@ app.get('/api/persons', (request, response) => {
 });
 
 app.get('/info', (request, response) => {
-  response.send(`<p>Phonebook has info for ${persons.length} people</p>${new Date()}<p></p>`);
+  Person.find({}).then(result => {
+    response.send(`<p>Phonebook has info for ${result.length} people</p>${new Date()}<p></p>`);
+  });
 });
 
 app.get('/api/persons/:id', (request, response, next) => {
@@ -109,8 +89,8 @@ app.put('/api/persons/:id', (request, response, next) => {
       context: 'query'
     })
     .then(updatedPerson => {
-        response.json(updatedPerson);
-      }
+      response.json(updatedPerson);
+    }
     )
     .catch(error => {
       next(error);
