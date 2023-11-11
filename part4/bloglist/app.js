99bddc2fe15c811fc18c21466c7f847c40a54ff6
Danny Sun <sunnyhust2005@qq.com>
2023-11-11 18:51:45 +0800

    Finishes 4.2 Blog list, step2

---
+++ part4/bloglist/app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const config = require('./utils/config');
+const mongoose = require('mongoose');
+const blogsRouter = require('./controllers/blogs');
+
+mongoose.set('strictQuery', false);
+
+mongoose.connect(config.MONGODB_URI)
+  .then(() => {
+    console.log('connected to MongoDB');
+  })
+  .catch((error) => {
+    console.log('error connecting to MongoDB:', error.message);
+  });
+
+app.use(cors());
+app.use(express.json());
+
+app.use('/api/blogs', blogsRouter);
+
+module.exports = app;
--- part4/bloglist/index.js
+++ part4/bloglist/index.js
@@ -1,26 +1,7 @@
-const express = require('express');
-const app = express();
-const cors = require('cors');
-const mongoose = require('mongoose');
-const blogsRouter = require('./controllers/blogs');
+const app = require('./app');
+const config = require('./utils/config');
 
-mongoose.set('strictQuery', false);
 
-const mongoUrl = 'mongodb+srv://sunnyhust2005:Db78398k@cluster0.qg2amgu.mongodb.net/bloglist?retryWrites=true&w=majority';
-mongoose.connect(mongoUrl)
-  .then(() => {
-    console.log('connected to MongoDB');
-  })
-  .catch((error) => {
-    console.log('error connecting to MongoDB:', error.message);
-  });
-
-app.use(cors());
-app.use(express.json());
-
-app.use('/api/blogs', blogsRouter);
-
-const PORT = 3003;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+app.listen(config.PORT, () => {
+  console.log(`Server running on port ${config.PORT}`);
 });
