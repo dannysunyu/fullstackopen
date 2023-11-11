29bed3acf732866d1c1133812f613ee357bfb691
Danny Sun <sunnyhust2005@qq.com>
2023-11-11 18:31:18 +0800

    Refactor with controller

--- part4/bloglist/index.js
+++ part4/bloglist/index.js
@@ -2,23 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const mongoose = require('mongoose');
-
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-});
-
-blogSchema.set('toJSON', {
-  transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString();
-    delete returnedObject._id;
-    delete returnedObject.__v;
-  }
-});
-
-const Blog = mongoose.model('Blog', blogSchema);
+const blogsRouter = require('./controllers/blogs');
 
 mongoose.set('strictQuery', false);
 
@@ -34,23 +18,7 @@ mongoose.connect(mongoUrl)
 app.use(cors());
 app.use(express.json());
 
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs);
-    });
-});
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body);
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result);
-    });
-});
+app.use('/api/blogs', blogsRouter);
 
 const PORT = 3003;
 app.listen(PORT, () => {
