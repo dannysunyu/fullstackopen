e5e3e11dc6f59ef6a816979dace3ade93803b987
Danny Sun <sunnyhust2005@qq.com>
2023-11-12 18:40:38 +0800

    Finishes 4.5*: helper functions and unit tests, step3

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -6,7 +6,27 @@ const totalLikes = (blogs) => {
   return blogs.map(blog => blog.likes).reduce((runningSum, num) => runningSum + num, 0);
 }
 
+const favoriteBlog = (blogs) => {
+  if (blogs.length === 0) {
+    return null;
+  }
+
+  let favoriteBlog = blogs[0];
+  for (let i = 1; i < blogs.length; i++) {
+    if (blogs[i].likes > favoriteBlog.likes) {
+      favoriteBlog = blogs[i];
+    }
+  }
+
+  return {
+    title: favoriteBlog.title,
+    author: favoriteBlog.author,
+    likes: favoriteBlog.likes,
+  };
+}
+
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog,
 }
