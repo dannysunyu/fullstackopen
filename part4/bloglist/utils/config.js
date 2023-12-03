83526ec65226b425533830b56290039cd968a7b2
Danny Sun <sunnyhust2005@qq.com>
2023-12-03 12:41:56 +0800

    Finishes 4.8 - Blog list tests step1

--- part4/bloglist/utils/config.js
+++ part4/bloglist/utils/config.js
@@ -1,7 +1,19 @@
 require('dotenv').config();
 
 const PORT = process.env.PORT;
-const MONGODB_URI = process.env.MONGODB_URI;
+
+const chooseMongoEnvironment = () => {
+  switch (process.env.NODE_ENV) {
+    case 'development':
+      return process.env.DEVELOPMENT_MONGODB_URI;
+    case 'test':
+      return process.env.TEST_MONGODB_URI;
+    default:
+      return process.env.MONGODB_URI;
+  }
+};
+
+const MONGODB_URI = chooseMongoEnvironment();
 
 module.exports = {
   MONGODB_URI,
