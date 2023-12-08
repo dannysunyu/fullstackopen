1db2c34e6597797e5f1952ac513f91fc9b1076d4
Danny Sun <sunnyhust2005@qq.com>
2023-12-08 15:45:45 +0800

    Finishes 4.20* - bloglist expansion, step8

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -5,7 +5,7 @@ const config = require('./utils/config');
 const mongoose = require('mongoose');
 const blogsRouter = require('./controllers/blogs');
 const usersRouter = require('./controllers/users');
-const { requestLogger, errorHandler, unknownEndpoint } = require('./utils/middleware');
+const { requestLogger, errorHandler, unknownEndpoint, tokenExtractor } = require('./utils/middleware');
 const loginRouter = require('./controllers/login');
 
 mongoose.set('strictQuery', false);
@@ -21,6 +21,7 @@ mongoose.connect(config.MONGODB_URI)
 app.use(cors());
 app.use(express.json());
 app.use(requestLogger);
+app.use(tokenExtractor);
 
 app.use('/api/blogs', blogsRouter);
 app.use('/api/users', usersRouter);
