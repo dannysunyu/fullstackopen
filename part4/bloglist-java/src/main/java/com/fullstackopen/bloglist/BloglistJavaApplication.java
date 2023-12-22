114c9a773bc55e85d1240b6423566f7be571c962
Danny Sun <sunnyhust2005@qq.com>
2023-12-22 23:09:38 +0800

    4.1 Blog list (Java), step1 - initial commit

---
+++ part4/bloglist-java/src/main/java/com/fullstackopen/bloglist/BloglistJavaApplication.java
@@ -0,0 +1,13 @@
+package com.fullstackopen.bloglist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BloglistJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BloglistJavaApplication.class, args);
+    }
+
+}
---
+++ part4/bloglist-java/src/test/java/com/fullstackopen/bloglist/BloglistJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fullstackopen.bloglist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BloglistJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
